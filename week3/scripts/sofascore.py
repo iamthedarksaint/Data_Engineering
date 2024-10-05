0dd439dc67a91890b3c8cb92d113d37be8ef4371
Darksaint <bojzino128@gmail.com>
2024-10-05 14:20:23 +0100

    Added week 4 assignment

--- week3/scripts/sofascore.py
+++ week3/scripts/sofascore.py
@@ -1,30 +1,47 @@
 import requests
-import http.client
 import json
 import pandas as pd
 
+def sofascore(url, headers) -> None:
+    try:
+        response = requests.get(url, headers=headers)
+        data = response.json()
+        print(json.dumps(data, indent=4))
+        return data
+        # json_data = json.loads(data.decode("utf-8"))
+        # data = pd.json_normalize(json_data)
+        # return data
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching data: {e}")
+        return None
 
-r =  requests.get("https://rapidapi.com/Creativesdev/api/free-football-api-data/playground/apiendpoint_3f3466e5-be4d-451b-bdec-eac888dbd88c")
-print(r.status_code)
+def parquet(data, parquet_file_path):
+    try:
+        df = pd.DataFrame(data)
+        df.to_parquet(parquet_file_path, engine="pyarrow")
+        print(f"Data saved as parquet file: {parquet_file_path}")
+    except Exception as e:
+        print(f"Error saving to parquet: {e}")
 
+def excel(data, xlsx_file_path):
+    try:
+        df = pd.DataFrame(data)
+        df.to_excel(xlsx_file_path, index=False, engine="openpyxl")
+        print(f"Data saved as Excel file: {xlsx_file_path}")
+    except Exception as e:
+        print(f"Error saving to Excel: {e}" )
 
-conn = http.client.HTTPSConnection("free-football-api-data.p.rapidapi.com")
+if __name__ == "__main__":
+    url = "https://tennis-api-atp-wta-itf.p.rapidapi.com/tennis/v2/atp/h2h/matches/5992/677/"
 
-headers = {
-    'x-rapidapi-key': "73da526a6dmsh74387eb97413c99p17fc16jsn94ac1b7306ac",
-    'x-rapidapi-host': "free-football-api-data.p.rapidapi.com"
-}
+    headers = {
+	"x-rapidapi-key": "73da526a6dmsh74387eb97413c99p17fc16jsn94ac1b7306ac",
+	"x-rapidapi-host": "tennis-api-atp-wta-itf.p.rapidapi.com"
+    }
 
-conn.request("GET", "/football-current-number-of-live", headers=headers)
-
-res = conn.getresponse()
-data = res.read()
-
-print(data.decode("utf-8"))
-
-json_data = json.loads(data.decode("utf-8"))
-df = pd.json_normalize(json_data)
-print(df)
-
-df.to_csv('livescores.csv', index=False)
-df.to_parquet('livescores.parquet', index=False)
+    data = sofascore(url, headers)
+    print(f"This is data: {data}")
+    if data:
+        parquet(data, 'week3/data/livescore.parquet')
+        excel(data, "week3/data/livescore.xlsx")
+    
